f2094345a1b45eda38d8bddb15f4f318c87c0b7e
Veljko <135993016+veniX902@users.noreply.github.com>
2023-07-19 19:09:58 +0200

    first commit

---
+++ FunctionApp1/Function1.cs
@@ -0,0 +1,36 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace FunctionApp1
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+        ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string temperature = req.Query["temperature"];
+
+            double celsius;
+            if (!double.TryParse(temperature, out celsius))
+            {
+                return new BadRequestObjectResult("Invalid temperature value");
+            }
+
+            double fahrenheit = (celsius * 9 / 5) + 32;
+
+            return new OkObjectResult($"{celsius} degrees Celsius is {fahrenheit} degrees Fahrenheit.");
+        }
+
+    }
+}
